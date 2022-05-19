59a98523425f3631723263a76873712135a1dac3
arvindholmes-asimo <arvind.holmes@gmail.com>
2022-05-19 19:17:33 +0530

    Initial commit

---
+++ big2no.js
@@ -0,0 +1,14 @@
+
+function largest(a, b){
+    if (a > b)
+        return a;
+    else if (a === b)
+        return ("both are equal");
+    else 
+    return b;
+  
+
+}
+var a = 150
+var b = 150
+console.log(largest(a, b))
---
+++ small12.js
@@ -0,0 +1,12 @@
+function smallest(x, y){
+    if (x<y)	
+    return x;
+    else if (x===y)
+    return "both are equal"
+    else 
+    return y;
+}
+  
+    var x= 75
+    var y = 475
+    console.log (smallest(x,y))
